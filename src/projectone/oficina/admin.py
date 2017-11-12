fd84f804074192acb4b5bc7bee45e838b6439936
Krix Apolinario <me@krixapolinar.io>
2017-11-12 01:59:19 -0300

    Create Models

--- src/projectone/oficina/admin.py
+++ src/projectone/oficina/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Vehicle, Product, Service, Quotation
+
+@admin.register(Client, Vehicle, Product, Service, Quotation)
+
+class ClientAdmin(admin.ModelAdmin):
+    fields = ('cpf', 'name', 'email', 'phonenumber', 'address_street', 'address_city', 'address_state')
+
+class VehicleAdmin(admin.ModelAdmin):
+    fields = ('license_plate', 'owner', 'brand', 'model', 'year', 'kilometer', 'chassis')
+
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('name', 'price', 'quantity', 'state', 'description')
+
+class ServiceAdmin(admin.ModelAdmin):
+    fields = ('name', 'price', 'description')
+
+class QuotationAdmin(admin.ModelAdmin):
+    fields = ('client', 'vehicle', 'validade', 'payment', 'items', 'services', 'description')
--- src/projectone/oficina/models.py
+++ src/projectone/oficina/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+  name = models.CharField('Nome Completo', max_length=100)
+  email = models.EmailField('E-Mail')
+  cpf = models.CharField('CPF', max_length=20)
+  phonenumber = models.CharField('Telefone', max_length=20)
+  address_street = models.CharField('Endereco', max_length=100)
+  address_city = models.CharField('Cidade', max_length=50)
+  address_state = models.CharField('Estado', max_length=50)
+  date_joined = models.DateTimeField('Adicionado em', auto_now_add=True)
+
+  def __str__(self):
+    return self.name
+
+class Vehicle(models.Model):
+  owner = models.ForeignKey(Client, on_delete=models.CASCADE)
+  brand = models.CharField('Marca', max_length=50)
+  model = models.CharField('Modelo', max_length=50)
+  year = models.CharField('Ano', max_length=4)
+  license_plate = models.CharField('Placa', max_length=8)
+  chassis = models.CharField('Chassi', max_length=50)
+  kilometer = models.CharField('Kilometragem', max_length=6)
+  date_joined = models.DateTimeField('Adicionado em', auto_now_add=True)
+
+  def __str__(self):
+    return self.license_plate
+
+class Product(models.Model):
+  STATE=(
+        ('new', 'Novo'),
+        ('used', 'Usado'),
+  )
+  name = models.CharField('Nome', max_length=50)
+  price = models.CharField('Valor', max_length=10)
+  quantity = models.CharField('Quantidade', max_length=4)
+  state = models.CharField('Estado', max_length=5, choices=STATE)
+  description = models.TextField('Descricao', max_length=200)
+
+  def __str__(self):
+    return self.name
+
+class Service(models.Model):
+  name = models.CharField('Nome', max_length=50)
+  price = models.CharField('Valor', max_length=10)
+  description = models.TextField('Descricao', max_length=200)
+
+  def __str__(self):
+    return self.name
+
+class Quotation(models.Model):
+  client = models.ForeignKey(Client, on_delete=models.CASCADE)
+  vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+  items = models.ForeignKey(Product, on_delete=models.CASCADE)
+  services = models.ForeignKey(Service, on_delete=models.CASCADE)
+  validade = models.CharField('Validade', max_length=2)
+  payment = models.CharField('Token',max_length=20)
+  description = models.TextField('Observacoes', max_length=200)
+  date = models.DateTimeField('Data', auto_now_add=True)
+  token = models.CharField('Token',max_length=20)
+
+  def __str__(self):
+    return self.id
