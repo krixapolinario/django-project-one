361f4a6f7e7deb55a416f836fe7c534a4ddbeee5
Krix Apolinario <me@krixapolinar.io>
2017-11-15 18:36:24 -0300

    Update Models and Admin

--- src/projectone/oficina/admin.py
+++ src/projectone/oficina/admin.py
@@ -2,19 +2,34 @@ from django.contrib import admin
 
 from .models import Client, Vehicle, Product, Service, Quotation
 
-@admin.register(Client, Vehicle, Product, Service, Quotation)
+#@admin.register(Client, Vehicle, Product, Service, Quotation)
 
 class ClientAdmin(admin.ModelAdmin):
-    fields = ('cpf', 'name', 'email', 'phonenumber', 'address_street', 'address_city', 'address_state')
+    fields = ['cpf', 'name', 'email', 'phonenumber', 'address_street', 'address_city', 'address_state']
+
+admin.site.register(Client, ClientAdmin)
 
 class VehicleAdmin(admin.ModelAdmin):
-    fields = ('license_plate', 'owner', 'brand', 'model', 'year', 'kilometer', 'chassis')
+    fields = ['license_plate', 'owner', 'brand', 'model', 'year', 'kilometer', 'chassis']
+
+admin.site.register(Vehicle, VehicleAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
-    fields = ('name', 'price', 'quantity', 'state', 'description')
+    fields = ['name', 'price', 'quantity', 'state', 'description']
+
+admin.site.register(Product, ProductAdmin)
 
 class ServiceAdmin(admin.ModelAdmin):
-    fields = ('name', 'price', 'description')
+    fields = ['name', 'price', 'description']
+
+admin.site.register(Service, ServiceAdmin)
 
 class QuotationAdmin(admin.ModelAdmin):
-    fields = ('client', 'vehicle', 'validade', 'payment', 'items', 'services', 'description')
+    fields = ['client', 'vehicle', 'validade', 'payment', 'items', 'services', 'description']
+
+admin.site.register(Quotation, QuotationAdmin)
+
+class ServiceOrderAdmin(admin.ModelAdmin):
+    fields = ['client', 'vehicle', 'validade', 'payment', 'items', 'services', 'description']
+
+admin.site.register(ServiceOrder, ServiceOrderAdmin)
