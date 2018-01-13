c7e3e9a3148fca06b0a09282e691a1af48c0b86e
Krix Apolinario <me@krixapolinar.io>
2018-01-13 09:59:34 -0300

    ...

--- src/projectone/projectone/urls.py
+++ src/projectone/projectone/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.loginview, name='login'),
     url(r'^login/', views.loginview, name='login'),
-    url(r'^oficina/', views.loginview, name='oficina'),
+    url(r'^oficina/', views.oficinaview, name='oficina'),
     url(r'^logout/', views.logoutview, name='logout'),
 ]
